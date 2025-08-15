9473e75508cd1f490e68cfe8faa2f7e56c74eb08
Rakeshattada123 <104977832+Rakeshattada123@users.noreply.github.com>
2025-08-15 17:14:48 +0530

    Update combined_main.py

--- combined_main.py
+++ combined_main.py
@@ -1,12 +1,10 @@
 from fastapi import FastAPI
-from project_ocr.main import app as ocr_app
 from project_rag.main import app as rag_app
 from project_transcription.main import app as transcription_app
 from project_webscrap.main import app as webscrap_app
 
 app = FastAPI(title="Combined AI Services")
 
-app.mount("/ocr", ocr_app)
 app.mount("/rag", rag_app)
 app.mount("/transcription", transcription_app)
 app.mount("/webscrap", webscrap_app)
